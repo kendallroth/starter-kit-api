6dc9a6282d31954caa1c1c9fe2659d819de9b420
Kendall Roth <kendall@kendallroth.ca>
2023-12-19 15:27:22 -0500

    Swagger doc pages (#4)
    * Improve build pipeline and file copying
    
    * GitHub Action for publishing Swagger doc

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,19 +1,31 @@
+import copyfiles, { type Options as CopyOptions } from "copyfiles";
 import { defineConfig } from "tsup";
 
 const production = process.env.NODE_ENV === "production";
 
 export default defineConfig((_options) => {
   return {
+    // NOTE: Cleaning build directory will also remove Swagger file if present!
     clean: true,
     dts: true,
     entry: {
       cli: "./src/cli.ts",
-      shared: "./src/shared.ts"
+      shared: "./src/shared.ts",
     },
     format: ["cjs", "esm"],
     splitting: production,
     metafile: !production,
     minify: production,
     outDir: "build",
+    onSuccess: async () => {
+      // Copy Swagger docs after bundling (ie. build/, docs/)
+      const swaggerPath = "./src/generated/swagger.json";
+      const options: CopyOptions = { up: 2 };
+      const noOp = () => {};
+
+      console.info("CLI Copying generated Swagger doc (after build)");
+      copyfiles([swaggerPath, "./build"], options, noOp);
+      copyfiles([swaggerPath, "./docs"], options, noOp);
+    },
   };
 });
