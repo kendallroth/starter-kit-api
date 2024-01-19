bc364eafc4e95ee266800fd1a9264f7b1eee1e3d
Kendall Roth <kendall@kendallroth.ca>
2024-01-19 09:12:22 -0500

    Add more generated accounts

--- src/resources/account/account.seed.ts
+++ src/resources/account/account.seed.ts
@@ -1,11 +1,28 @@
+import { faker } from "@faker-js/faker";
 import dayjs from "dayjs";
 
-import { mapToArray } from "#common/utilities";
+import { generateRandomList, mapToArray, randomBool } from "#common/utilities";
 import { AccountEntity, stubAccount } from "./account.entity";
 
 export const seedAccountIds = {
   user: "71800e2e-33a1-4413-abd4-45ea1671f911",
   other: "2879f8a9-ec66-4e65-863b-b02e79ec4031",
+};
+
+const generateAccount = (): AccountEntity => {
+  const createdAt = faker.date.past({ years: 2 });
+
+  return {
+    ...stubAccount({
+      createdAt: createdAt.toISOString(),
+      email: faker.internet.email(),
+      name: `${faker.person.firstName()} ${faker.person.lastName()}`,
+      password: faker.internet.password({ length: 24 }),
+      verifiedAt: randomBool(0.75)
+        ? faker.date.between({ from: createdAt, to: new Date() }).toISOString()
+        : null,
+    }),
+  };
 };
 
 const seedAccounts = (): Map<string, AccountEntity> => {
@@ -25,6 +42,7 @@ const seedAccounts = (): Map<string, AccountEntity> => {
       password: "password",
       createdAt: dayjs().subtract(3, "day").toISOString(),
     }),
+    ...generateRandomList(20, generateAccount),
   ];
 
   return new Map(accountList.map((a) => [a.id, a]));
